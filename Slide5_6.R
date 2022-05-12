51c0a7b1899d9b68a29a04c4d494bafce42133b5
nujcharee <s_nujcharee@hotmail.com>
2022-05-12 11:07:07 +0100

    Update Slide5_6.R

--- Slide5_6.R
+++ Slide5_6.R
@@ -70,82 +70,82 @@ lookup = readr::read_csv("https://raw.githubusercontent.com/nujcharee/DSDA_Data/
 shp = shp %>% left_join(pop %>% dplyr::select(Code, Population),by = c("id"="Code"))
 shp = shp %>% left_join(lookup %>% dplyr::select(LAD19CD, RGN19NM),by = c("id"="LAD19CD"))
 
-#-3.396750623	0.033481889 (higher risk)
-#-4.146532043	0.015819182 (lower risk)
-
 # top 10
 top_10 = CT0467 %>%
-    left_join(lookup, by=c("LA code"="LAD19CD")) %>%
-    mutate(prop = exp(`Prediction of loneliness`)) %>%
-    top_n(10,`Prediction of loneliness`) %>%
-    ggplot(., aes(x = reorder(`LA name`, `Prediction of loneliness`), y = exp(`Prediction of loneliness`)*100),fill=`LA name`) +
-    labs(title = "Top 10 highest relative risk of loneliness"
-         , subtitle = "Seen in multiple regions in England\n"
-         , caption = "Note: Relative risk in the scale of 1-3 where 3 being the higest and 1 lowest"
-         , y="", x="") +
-    geom_bar(stat = "identity", fill= c("#E40046"
-                                        , "#E40046"
-                                        , "#E40046"
-                                        , "#582C83"
-                                        , "#00A5DF"
-                                        , "#1D57A5"
-                                        , "#1D57A5"
-                                        , "#1D57A5"
-                                        , "#1D57A5"
-                                        , "#1D57A5")) +
-    coord_flip() +
-    geom_text(aes(label=RGN19NM), colour="white", size = 4, position = position_stack(vjust = 0.5)) +
-    theme(
-        text = element_text(color = "#22211d"),
-        plot.background = element_rect(fill = "#f5f5f2", color = NA),
-        panel.background = element_rect(fill = "#f5f5f2", color = NA),
-        legend.background = element_rect(fill = "#f5f5f2", color = NA),
-        #axis.text.x=element_blank(),
-        plot.title = element_text(size= 20, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
-        plot.subtitle = element_text(size= 12, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
-        plot.caption = element_text( size=8, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+  left_join(lookup, by=c("LA code"="LAD19CD")) %>%
+  mutate(prop = exp(`Prediction of loneliness`)) %>%
+  top_n(10,`Prediction of loneliness`) %>%
+  ggplot(., aes(x = reorder(`LA name`, `Prediction of loneliness`), y = exp(`Prediction of loneliness`)*100),fill=`LA name`) +
+  labs(title = "Top 10 highest relative risk of loneliness"
+       , subtitle = "Seen in multiple regions in England\n Relative risk in the scale of 1-3 where 3 being the highest and 1 lowest\n"
+       , x="", y="") +
+  geom_bar(stat = "identity", fill= c("#E40046"
+                                      , "#E40046"
+                                      , "#E40046"
+                                      , "#582C83"
+                                      , "#00A5DF"
+                                      , "#1D57A5"
+                                      , "#1D57A5"
+                                      , "#1D57A5"
+                                      , "#1D57A5"
+                                      , "#1D57A5")) +
+  coord_flip() +
+  scale_y_continuous(limits = c(0,3.5), expand = c(0, 0)) +
+  geom_text(aes(label=RGN19NM), colour="white", size = 4, position = position_stack(vjust = 0.5)) +
+  
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    #axis.text.x=element_blank(),
+    plot.title = element_text(size= 20, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+    plot.subtitle = element_text(size= 12, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+    plot.caption = element_text( size=8, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+    legend.position = c(0.7, 0.09)
+  )
 
-        legend.position = c(0.7, 0.09)
-    )
+top_10
 
 ## bottom 10
 
 bom_10 = CT0467 %>%
-    left_join(lookup, by=c("LA code"="LAD19CD")) %>%
-    mutate(prop = exp(`Prediction of loneliness`)) %>%
-    top_n(-10,`Prediction of loneliness`) %>%
-    ggplot(., aes(x = reorder(`LA name`, `Prediction of loneliness`), y = exp(`Prediction of loneliness`)*100),fill=`LA name`) +
-    labs(title = "Bottom 10 highest relative risk of loneliness"
-         , subtitle = "South East has lowest risk overall\nException is Rutland - the smallest local authority in England\n"
-         , caption = "Note: Relative risk in the scale of 0-1 where 1 being the higest and 0 lowest"
-         , y="", x="") +
-    coord_flip() +
-    geom_bar(stat = "identity", fill= c("#1D57A5"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00"
-                                        , "#84BD00")) +
+  left_join(lookup, by=c("LA code"="LAD19CD")) %>%
+  mutate(prop = exp(`Prediction of loneliness`)) %>%
+  top_n(-10,`Prediction of loneliness`) %>%
+  ggplot(., aes(x = reorder(`LA name`, `Prediction of loneliness`), y = exp(`Prediction of loneliness`)*100),fill=`LA name`) +
+  labs(title = "Bottom 10 lowest relative risk of loneliness"
+       , subtitle = "South East has lowest risk overall\nException is Rutland - the smallest local authority in England\nRelative risk in the scale of 0-1 where 1 being the higest and 0 lowest\n"
+       , y="", x="") +
+  geom_bar(stat = "identity", fill= c("#1D57A5"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00"
+                                      , "#84BD00")) +
+  coord_flip() +
+  scale_y_continuous(limits = c(0,1.6), expand = c(0, 0)) +
+  geom_text(aes(label=RGN19NM), colour="white", size = 4, position = position_stack(vjust = 0.5)) +
+  theme(
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    text = element_text(color = "#22211d"),
+    plot.background = element_rect(fill = "#f5f5f2", color = NA),
+    panel.background = element_rect(fill = "#f5f5f2", color = NA),
+    legend.background = element_rect(fill = "#f5f5f2", color = NA),
+    #axis.text.x=element_blank(),
+    plot.title = element_text(size= 20, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
+    plot.subtitle = element_text(size= 12, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
+    plot.caption = element_text( size=8, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
+    
+    legend.position = c(0.7, 0.09)
+    
+  )
 
-    geom_text(aes(label=RGN19NM), colour="white", size = 4, position = position_stack(vjust = 0.5)) +
-    theme(
-        text = element_text(color = "#22211d"),
-        plot.background = element_rect(fill = "#f5f5f2", color = NA),
-        panel.background = element_rect(fill = "#f5f5f2", color = NA),
-        legend.background = element_rect(fill = "#f5f5f2", color = NA),
-        #axis.text.x=element_blank(),
-        plot.title = element_text(size= 20, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.4, l = 2, unit = "cm")),
-        plot.subtitle = element_text(size= 12, hjust=0.01, color = "#4e4d47", margin = margin(b = -0.1, t = 0.43, l = 2, unit = "cm")),
-        plot.caption = element_text( size=8, color = "#4e4d47", margin = margin(b = 0.3, r=-99, unit = "cm") ),
-
-        legend.position = c(0.7, 0.09)
-
-    )
-
-top_10
 bom_10
-
